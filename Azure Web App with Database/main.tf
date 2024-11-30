42e84317ba773d7bd15be21c38368b2e1de2cb56
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-11-30 11:56:22 +0200

    update

--- Azure Web App with Database/main.tf
+++ Azure Web App with Database/main.tf
@@ -48,8 +48,28 @@ resource "azurerm_linux_web_app" "alwa" {
   connection_string {
     name  = "DefaultConnection"
     type  = "SQLAzure"
-    value = "Data Source=tcp:${fully qualified domain name of the MSSQL server},1433;Initial Catalog=${name of the SQL database};User ID=${username of the MSSQL server administrator};Password=${password of the MSSQL server administrator};Trusted_Connection=False; MultipleActiveResultSets=True;"
+    value = "Data Source=tcp:${azurerm_mssql_server.mssqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.mssqldatabase.name};User ID=${azurerm_mssql_server.mssqlserver.administrator_login};Password=${azurerm_mssql_server.mssqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
   }
+}
+
+resource "azurerm_mssql_server" "mssqlserver" {
+  name                         = "taskboardmssqlserver"
+  resource_group_name          = azurerm_resource_group.arg.name
+  location                     = azurerm_resource_group.arg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+
+resource "azurerm_mssql_database" "mssqldatabase" {
+  name         = "taskboard-db"
+  server_id    = azurerm_mssql_server.mssqlserver.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  zone_redundant = true
 }
 
 resource "azurerm_app_service_source_control" "example" {
